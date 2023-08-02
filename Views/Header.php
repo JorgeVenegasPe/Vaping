3948f6eb052f62f3a66a9fc9bb3d5b4234b8f3cf
HansCR68 <139811850+HansCR68@users.noreply.github.com>
2023-08-02 11:35:32 -0500

    HansActualizacion3.1

--- Views/Header.php
+++ Views/Header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="../Issets/css/header.css">
     <script src="https://kit.fontawesome.com/b408879b64.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
@@ -13,7 +13,7 @@
     <header class="header-container">
     <!-- Logo a la izquierda -->
     <div class="logo">
-        <img src="img/logooriginal1.png" alt="Logo">
+        <img src="../Issets/img/LogoTipo/logooriginal1.png" alt="Logo">
     </div>
     
     <!-- Barra de búsqueda a la derecha -->
