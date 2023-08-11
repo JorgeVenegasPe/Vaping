bf28e577f4334bd93bf63d4b8732eb324fb99336
JimyJampier18 <140426742+JimyJampier18@users.noreply.github.com>
2023-08-11 12:06:19 -0500

    ActualizacionJimy03

--- DataBase/vaping.sql
+++ DataBase/vaping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-07-2023 a las 17:34:11
+-- Tiempo de generación: 07-08-2023 a las 16:48:34
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,15 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `pods`
+-- Estructura de tabla para la tabla `categoria`
 --
 
-CREATE TABLE `pods` (
-  `IdPods` int(11) NOT NULL,
+CREATE TABLE `categoria` (
+  `IdCategoria` int(11) NOT NULL,
+  `NombreCategoria` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`IdCategoria`, `NombreCategoria`) VALUES
+(1, 'Pods'),
+(2, 'Vapers');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `IdProducto` int(11) NOT NULL,
+  `IdCategoria` int(11) NOT NULL,
   `Nombre` varchar(50) NOT NULL,
-  `Imagen` varchar(50) NOT NULL,
-  `Descripcion` varchar(50) NOT NULL,
-  `Precio` int(20) NOT NULL
+  `imagen` longblob DEFAULT NULL,
+  `Descripcion` varchar(200) NOT NULL,
+  `Precio` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -48,29 +68,22 @@ CREATE TABLE `usuarios` (
   `Contrasena` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `vapers`
---
-
-CREATE TABLE `vapers` (
-  `IdVapers` int(11) NOT NULL,
-  `Nombre` varchar(50) NOT NULL,
-  `Imagen` varchar(50) NOT NULL,
-  `Descripcion` varchar(50) NOT NULL,
-  `Precio` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `pods`
+-- Indices de la tabla `categoria`
 --
-ALTER TABLE `pods`
-  ADD PRIMARY KEY (`IdPods`);
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`IdCategoria`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`IdProducto`),
+  ADD KEY `fk_categoria_producto` (`IdCategoria`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -79,32 +92,26 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`IdUsuarios`);
 
 --
--- Indices de la tabla `vapers`
---
-ALTER TABLE `vapers`
-  ADD PRIMARY KEY (`IdVapers`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `pods`
+-- AUTO_INCREMENT de la tabla `categoria`
 --
-ALTER TABLE `pods`
-  MODIFY `IdPods` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `categoria`
+  MODIFY `IdCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `IdProducto` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `IdUsuarios` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `vapers`
---
-ALTER TABLE `vapers`
-  MODIFY `IdVapers` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Views/Pods.php
+++ Views/Pods.php
@@ -5,34 +5,90 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Vaping - Pods</title>
+  <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" />
   <link rel="stylesheet" href="../Issets/css/main.css"/>
-  
-
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
 </head>
 <body>
-  
+  <!-- Header  -->
+    <?php
+    require("../Controllers/ControllerProducto.php");
+    $obj = new usernameControlerProducto();
+    $rows = $obj->VerProductoPods();
+    require 'Header.php'
+    ?>
+
   <!-- Carrusel  -->
-  <main>
-  <button id="loginButton">Iniciar sesión</button>
+  <main class="main">
+    <div class="swiper carousel">
+      <div class="swiper-wrapper">
+        <?php if ($rows):?>
+            <?php foreach ($rows as $row): ?>
+
+              <div class="swiper-slide">
+                <div class="imagenpods">
+                  <img height="50px" src="data:image/webp;base64,<?= base64_encode($row[3]) ?>"/>
+                  <div class="container-descrip">
+                    <h1><?=$row[2]?></h1>
+                    <p><?=$row[4]?></p>
+                    <h2>S/.<?=$row[5]?></h2>
+                    <div class="container-butt">
+                      <button type="button" class="button-cantidad Añadir" >Añadir</button>
+                      <button type="button" class="button-cantidad">Cantidad</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <?php endforeach;?>
+        <?php endif;?>
+
+      </div>
+        <button type="button" class="swiper-button-next"></button>
+        <button type="button" class="swiper-button-prev"></button>
+    </div>
   </main>
   
   <!-- Carrito  -->
   <div>
-
   </div>
 
-  <!-- Modals  -->
-  <div id="loginModal" style="display: none;">
-  <?php
-    require 'Login.php';
-  ?>
-    
+  <!-- ================ Modals ================================  -->
+    <!-- Login  -->
+    <div id="loginModal" style="display: none;">
+      <?php
+        require 'Login.php';
+      ?>
+    </div>
 
-</div>
-
+  <!-- Redes Sociales -->
+  <div class="icons-redes">
+    <a href="#" class="icon icon--instagram">
+      <i class="ri-instagram-line"></i>
+    </a>
+    <a href="#" class="icon icon--facebook">
+      <i class="ri-facebook-line"></i>
+    </a>
+    <a href="#" class="icon icon--whatsapp">
+      <i class="ri-whatsapp-line"></i>
+    </a>
   </div>
-  <script>
-    const loginButton = document.getElementById('loginButton');
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<script src="../Issets/js/main.js"></script>
+<script>
+  var swiper = new Swiper(".carousel", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+
+  const loginButton = document.getElementById('loginButton');
     const loginModal = document.getElementById('loginModal');
 
   // Asigna un controlador de eventos al botón de inicio de sesión
@@ -40,12 +96,6 @@
   // Muestra el modal
     loginModal.style.display = 'block';
     });
-  </script>
-
-  <!-- Redes Sociales -->
-  <div>
-
-  </div>
-  
+</script>
 </body>
 </html>
